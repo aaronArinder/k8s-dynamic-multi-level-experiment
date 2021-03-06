d018dc1dbe2fdd1c088180b0d338bfeb7ce8bef1
Aaron Arinder <aaronarinder@gmail.com>
2021-03-06 13:37:10 -0500

    init commit: dockerfile and simple-server

---
+++ src/simple-server.go
@@ -0,0 +1,19 @@
+// +build ignore
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main(){
+    http.HandleFunc("/", handler)
+    // http.ListenAndServe blocks until error
+    // that's why it's returned to log.Fatal()
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
