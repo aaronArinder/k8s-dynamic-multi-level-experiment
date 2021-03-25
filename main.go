8abb1fe002f114c76679fd65061aca2ce77b1533
Aaron Arinder <aaronarinder@gmail.com>
2021-03-25 13:33:43 -0400

    cpu crunch in main.go

--- main.go
+++ main.go
@@ -2,13 +2,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"runtime"
 	"time"
 )
 
+var count = 0;
 func handler(w http.ResponseWriter, r *http.Request) {
-    time.Sleep(1 * time.Second)
+    f, err := os.Open(os.DevNull)
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+
+    n := runtime.NumCPU()
+    runtime.GOMAXPROCS(n)
+
+    for i := 0; i < n; i++ {
+        go func() {
+            for {
+                fmt.Fprintf(f, ".")
+            }
+        }()
+    }
+
+    time.Sleep(2 * time.Second)
+    count += 1
+    log.Printf("finished %d request", count)
 }
 
 func main(){
